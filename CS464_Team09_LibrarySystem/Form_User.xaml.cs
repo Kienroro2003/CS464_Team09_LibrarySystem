e9e89dcb2bda6ea513d635bf411913d602244974
dinhthi259 <nguyendinhthi25904@gmail.com>
2025-09-15 16:17:41 +0700

    Add project files.

---
+++ CS464_Team09_LibrarySystem/Form_User.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CS464_Team09_LibrarySystem
+{
+    /// <summary>
+    /// Interaction logic for Form_User.xaml
+    /// </summary>
+    public partial class Form_User : Window
+    {
+        public Form_User()
+        {
+            InitializeComponent();
+        }
+    }
+}

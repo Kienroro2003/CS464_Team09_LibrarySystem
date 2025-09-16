76774885d2023847e6b173d12c45975513d11994
Kienroro2003 <nguyentrankien281@gmail.com>
2025-09-16 09:38:54 +0700

    macos

--- RentalHistory.xaml.cs
+++ RentalHistory.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace LS
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for RentalHistory.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class RentalHistory : Window
     {
-        public MainWindow()
+        public RentalHistory()
         {
             InitializeComponent();
         }
